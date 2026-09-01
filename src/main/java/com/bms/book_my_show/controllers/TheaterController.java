d5ae5ec7a55ea38d79abbccd11b2401244dd7dda
HRISHI <hrishikesh77318@gmail.com>
2026-09-01 18:14:01 +0530

    used sql query to insert operations

--- src/main/java/com/bms/book_my_show/controllers/TheaterController.java
+++ src/main/java/com/bms/book_my_show/controllers/TheaterController.java
@@ -2,7 +2,11 @@ package com.bms.book_my_show.controllers;
 
 import com.bms.book_my_show.constants.LoggerConstant;
 import com.bms.book_my_show.dtos.RegisterCompanyDto;
+import com.bms.book_my_show.models.Company;
+import com.bms.book_my_show.services.TheaterService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/theater")
 public class TheaterController {
 
+    private TheaterService theaterService;
+
+    @Autowired
+    public TheaterController(TheaterService theaterService){
+        this.theaterService=theaterService;
+    }
+
     @PostMapping("/company/register")
     public ResponseEntity registerTheaterCompany(@RequestBody RegisterCompanyDto registerCompanyDto){
         log.info(String.format(LoggerConstant.REQUEST_RECEIVED_MESSAGE,"register company",registerCompanyDto.toString()));
+        Company theaterCompany= theaterService.registerTheaterCompany(registerCompanyDto);
+
+        return new ResponseEntity<>(theaterCompany, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/bms/book_my_show/services/TheaterService.java
+++ src/main/java/com/bms/book_my_show/services/TheaterService.java
@@ -1,9 +1,23 @@
 package com.bms.book_my_show.services;
 
+import com.bms.book_my_show.dtos.RegisterCompanyDto;
+import com.bms.book_my_show.enums.CompanyType;
+import com.bms.book_my_show.models.Company;
+import com.bms.book_my_show.models.Theater;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TheaterService {
 
+    private CompanyService companyService;
 
+    @Autowired
+    public TheaterService(CompanyService companyService){
+        this.companyService=companyService;
+    }
+
+    public Company registerTheaterCompany(RegisterCompanyDto registerCompanyDto){
+        return companyService.registerTheaterCompany(registerCompanyDto, CompanyType.THEATER_COMPANY);
+    }
 }
